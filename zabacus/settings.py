c9e11a11f1c3c61ac5e8fd036eb7479695f00204
Yihe Huang <yihehuang@g.harvard.edu>
2018-10-05 01:02:16 -0400

    cors header

--- zabacus/settings.py
+++ zabacus/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'zabacus.bills',
     'graphene_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +132,5 @@ STATIC_URL = '/static/'
 GRAPHENE = {
     'SCHEMA': 'zabacus.schema.schema'
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
